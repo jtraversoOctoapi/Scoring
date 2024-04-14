8bcb2c807ad94c1b79d569a88c0c1ce1a7e294f9
jtraversoOctoapi <167014703+jtraversoOctoapi@users.noreply.github.com>
2024-04-14 16:43:29 -0400

    Update main.py

--- functions/fetchQuery/src/main.py
+++ functions/fetchQuery/src/main.py
@@ -3,15 +3,15 @@ from appwrite.services.databases import Databases
 
 def main(context):
     if context.req.method == 'GET':        
-        try:
-            # Initialize the Appwrite client
-            client = Client()
-            client.set_endpoint('https://cloud.appwrite.io/v1') \
-                  .set_project('661bf232a2d367eccb49') \
-                  .set_key('68a4559f483223bde0c57f0630be9b37191a1a809353da2538a1dcbc3da07039cdf6e9e09a62ae01baa5f026b241981f7cf95ea4be884a12cd95b23d22baae2cc25540fe12cb9655224b0d5c8b7d7022098e7afa558724718032854c413b0d25d08cd6c8615c61e611c6ad83b225f61e4a5bea40ef1b8b4573343bfea693d58a')
+    try:
+        # Initialize the Appwrite client
+        client = Client()
+        client.set_endpoint('https://cloud.appwrite.io/v1') \
+            .set_project('661bf232a2d367eccb49') \
+            .set_key('68a4559f483223bde0c57f0630be9b37191a1a809353da2538a1dcbc3da07039cdf6e9e09a62ae01baa5f026b241981f7cf95ea4be884a12cd95b23d22baae2cc25540fe12cb9655224b0d5c8b7d7022098e7afa558724718032854c413b0d25d08cd6c8615c61e611c6ad83b225f61e4a5bea40ef1b8b4573343bfea693d58a')
 
-            # Extract the document ID from the path
-            path_parts = context.req.path.split('/')
+         # Extract the document ID from the path
+        path_parts = context.req.path.split('/')
             if len(path_parts) == 3 and path_parts[1] == 'documents':
                 document_id = path_parts[2]
                 # Initialize the database client
@@ -20,6 +20,5 @@ def main(context):
                 return context.res.json(result)
             else:
                 return context.res.json({'message': 'Invalid path'}, status_code=404)
-    
-        except Exception as e:
-            return context.res.json({'error': str(e)}, status_code=500)
+    except Exception as e:
+        return context.res.json({'error': str(e)}, status_code=500)
