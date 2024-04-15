fc4930808b8986408a9204d2208638f9ca6f39c9
jtraversoOctoapi <167014703+jtraversoOctoapi@users.noreply.github.com>
2024-04-15 01:37:20 -0400

    Update main.py

--- function/ScoringDemo/src/main.py
+++ function/ScoringDemo/src/main.py
@@ -17,14 +17,13 @@ client.set_endpoint('https://cloud.appwrite.io/v1') \
 database = Databases(client)
 
 def main(context):
-    content_type = context.req.headers.get('content-type', '').lower()
     path_parts = context.req.path.split('/')
+    content_type = context.req.headers.get('content-type', '').lower()
+    
     if context.req.method == 'GET':
         if len(path_parts) == 1:
-            # Servir la página HTML principal si no hay más partes en la ruta
             return context.res.send(html_template, 200, {'Content-Type': 'text/html'})
         elif len(path_parts) == 3 and path_parts[1] == 'documents':
-            # Buscar y devolver un documento si la ruta es del tipo /documents/{documentId}
             document_id = path_parts[2]
             try:
                 result = database.get_document('661c0ff748205b5d00b5', '661c1000c15d1c28d50a', document_id)
@@ -33,36 +32,31 @@ def main(context):
                 return context.res.json({'error': str(e)}, 500)
         else:
             return context.res.json({'message': 'Invalid path'}, 400)
-    
+
     elif context.req.method == 'POST' and 'application/x-www-form-urlencoded' in content_type:
         formData = parse_qs(context.req.body)
         rut = formData.get('rut', [''])[0]
         email = formData.get('email', [''])[0]
-        
-        # Crea un nuevo documento en la colección
+
         document = database.create_document(
             database_id='661c0ff748205b5d00b5',
             collection_id='661c1000c15d1c28d50a',
             document_id=ID.unique(), 
-            data={'rut': rut, 'email': email},
+            data={'rut': rut, 'email': email}
         )
-        
-        # Extrae el ID del nuevo documento
+
         document_id = document['$id']
         created_at = document['$createdAt']
-        
-        # Datos para enviar al webhook, incluyendo el ID del documento
+
         data_to_send = {
             'rut': rut,
             'email': email,
             'document_id': document_id,
             'created_at': created_at
         }
-        
-        # URL del webhook
+
         webhook_url = 'https://hook.us1.make.com/iq7pr5ib62ct8pin9qxwsxf5jh2v3w3m'
         try:
-            # Enviar los datos al webhook y capturar la respuesta
             response = requests.post(webhook_url, json=data_to_send)
             if response.status_code == 200:
                 return context.res.json({
@@ -73,14 +67,13 @@ def main(context):
                     "created_at": created_at
                 }, 200)
             else:
-                return context.res.json({"error": "Error en el proceso del webhook.", "status_code": response.status_code}, status=response.status_code)
+                return context.res.json({"error": "Error en el proceso del webhook.", "status_code": response.status_code}, response.status_code)
         except Exception as e:
             return context.res.json({"error": "Ocurrió un error al procesar la solicitud", "message": str(e)}, 500)
     else:
-        error_message = {
+        return context.res.json({
             "error": True,
             "message": "Método o tipo de contenido no permitido.",
             "method_received": context.req.method,
             "content_type_received": content_type
-        }
-        return context.res.json(error_message, 400)
+        }, 400)
