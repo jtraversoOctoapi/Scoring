e4d63acfc5664c77faa239106d3f424c1fbf3c4c
jtraversoOctoapi <167014703+jtraversoOctoapi@users.noreply.github.com>
2024-04-14 22:12:49 -0400

    Update templates.py

--- function/ScoringDemo/src/templates.py
+++ function/ScoringDemo/src/templates.py
@@ -1,97 +1,89 @@
 html_template = '''
 <!doctype html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DEMO SCORING</title>
     <link rel="stylesheet" href="https://unpkg.com/pico.css">
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        function openModal(response) {
-            document.getElementById('responseText').value = response;
-            document.getElementById('modal').style.display = 'flex';
-        }
-
-        function closeModal() {
-            document.getElementById('modal').style.display = 'none';
-        }
-
-        function formDataToUrlEncoded(formElement) {
-            const formData = new FormData(formElement);
-            const pairs = [];
-            for (const [key, value] of formData) {
-                pairs.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`);
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            function openModal(response) {
+                document.getElementById('responseText').value = response;
+                document.getElementById('modal').style.display = 'flex';
             }
-            return pairs.join('&');
-        }
 
-        document.querySelector('form').addEventListener('submit', function (e) {
-            e.preventDefault();
-            var formData = new FormData(this);
-            console.log('URL de acción:', this.action);
-            const urlEncodedData = formDataToUrlEncoded(this);
-            for (var pair of formData.entries()) {
-                console.log(pair[0]+ ': ' + pair[1]);  // Muestra cada campo del formulario y su valor
+            function closeModal() {
+                document.getElementById('modal').style.display = 'none';
             }
-            document.getElementById('loader').style.display = 'block'; // Mostrar el loader
-            console.log('Esperando el resultado de su evaluación...'); // Añadir console.log para depuración
-            
-            fetch(this.action, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: urlEncodedData
-            })
-            .then(response => {
-                console.log('Respuesta recibida', response);
-                console.log('Estado de la respuesta:', response.status);
-                if (response.ok) {
-                    response.text().then(text => {
-                        console.log('Respuesta como texto:', text);
-                         return response.text();  // Asumimos que la respuesta es texto y no JSON
-                    });
-                } else {
-                    response.text().then(text => {
-                        console.error('Error en la respuesta:', text);
-                        throw new Error('Respuesta del servidor no contiene el formato correcto');
-                    });
-                }
-            })
-            .then(data => {
-                console.log('Datos recibidos', data); // Añadir console.log para ver los datos recibidos
-                if (data && data.document_id) {
-                    checkForResponse(data.document_id); // Comienza a verificar la respuesta
-                } else {
-                    console.error('document_id no está presente en la respuesta');
-                }
-            })
-            .catch(error => {
-                console.error('Error al enviar formulario:', error);
-            });
-        });
-    });
 
-    function checkForResponse(documentId) {
-        console.log('Verificando respuesta para el documento', documentId); // Añadir console.log para depuración
-        const interval = setInterval(() => {
-            fetch(`https://661c32a7cbb49de418a6.appwrite.global/documents/${documentId}`)
-            .then(response => response.json())
-            .then(data => {
-                console.log('Verificación de datos', data); // Añadir console.log para ver los datos de la verificación
-                if (data.respuesta !== null) {
-                    clearInterval(interval); // Detiene las comprobaciones
-                    document.getElementById('loader').style.display = 'none'; // Ocultar el loader
-                    openModal(data.respuesta); // Abre el modal con la respuesta
+            function formDataToUrlEncoded(formElement) {
+                const formData = new FormData(formElement);
+                const pairs = [];
+                for (const [key, value] of formData) {
+                    pairs.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`);
                 }
-            })
-            .catch(error => {
-                console.error('Error al consultar el documento:', error);
+                return pairs.join('&');
+            }
+
+            document.querySelector('form').addEventListener('submit', function (e) {
+                e.preventDefault();
+                var formData = new FormData(this);
+                console.log('URL de acción:', this.action);
+                const urlEncodedData = formDataToUrlEncoded(this);
+
+                document.getElementById('loader').style.display = 'block'; // Mostrar el loader
+                console.log('Esperando el resultado de su evaluación...'); // Añadir console.log para depuración
+
+                fetch(this.action, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    body: urlEncodedData
+                })
+                    .then(response => {
+                        if (response.ok) {
+                            return response.json();
+                        } else {
+                            return response.text().then(text => Promise.reject('Error en la respuesta: ' + text));
+                        }
+                    })
+                    .then(data => {
+                        if (data.document_id) {
+                            console.log('Datos recibidos:', data);
+                            checkForResponse(data.document_id);  // Usamos document_id de la respuesta
+                        } else {
+                            console.error('Document_id no está presente en la respuesta');
+                            document.getElementById('loader').style.display = 'none'; // Ocultar el loader si hay un error
+                            alert('Ha ocurrido un error: ' + error); // Mostrar error al usuario
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Error al enviar formulario:', error);
+                    });
             });
-        }, 2000); // Consulta cada 2 segundos
-    }
-</script>
+
+            function checkForResponse(documentId) {
+                console.log('Verificando respuesta para el documento', documentId); // Añadir console.log para depuración
+                const interval = setInterval(() => {
+                    fetch(`https://661c32a7cbb49de418a6.appwrite.global/documents/${documentId}`)
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log('Verificación de datos', data); // Añadir console.log para ver los datos de la verificación
+                            if (data.respuesta !== null) {
+                                clearInterval(interval); // Detiene las comprobaciones
+                                document.getElementById('loader').style.display = 'none'; // Ocultar el loader
+                                openModal(data.respuesta); // Abre el modal con la respuesta
+                            }
+                        })
+                        .catch(error => {
+                            console.error('Error al consultar el documento:', error);
+                        });
+                }, 2000); // Consulta cada 2 segundos
+            }
+    </script>
     <style>
         body,
         html {
@@ -106,6 +98,9 @@ html_template = '''
         }
 
         .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
             width: 100%;
             max-width: 330px;
             padding: 15px;
@@ -177,6 +172,10 @@ html_template = '''
         }
 
         #loader {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
             position: fixed;
             left: 50%;
             top: 50%;
@@ -187,9 +186,12 @@ html_template = '''
             padding: 20px;
             border-radius: 10px;
             z-index: 1000;
-            /* Añade un borde y animación */
+        }
+
+        .loader-circle {
             border: 6px solid #f3f3f3;
             border-top: 6px solid #3498db;
+            border-radius: 50%;
             width: 50px;
             height: 50px;
             animation: spin 2s linear infinite;
@@ -197,11 +199,20 @@ html_template = '''
 
         /* Animación de giro */
         @keyframes spin {
-            0% { transform: rotate(0deg); }
-            100% { transform: rotate(360deg); }
+            0% {
+                transform: rotate(0deg);
+            }
+
+            100% {
+                transform: rotate(360deg);
+            }
         }
+
+        /* Asegúrese de que el texto "Cargando..." esté centrado en el contenedor */
+        .loader-text {
     </style>
 </head>
+
 <body>
     <div class="modal" id="modal">
         <div class="modal-content">
@@ -211,7 +222,8 @@ html_template = '''
         </div>
     </div>
     <div id="loader" style="display: none;">
-        Cargando...
+        <div class="loader-circle"></div>
+        <div class="loader-text">Esperando el resultado de tu evaluación...</div>
     </div>
     <div class="container">
         <h1>DEMO SCORING</h1>
@@ -228,5 +240,6 @@ html_template = '''
         </form>
     </div>
 </body>
+
 </html>
 '''
