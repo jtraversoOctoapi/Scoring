edf6c70d69c1d82a36d10cddeae6df755d31f3a6
jtraversoOctoapi <167014703+jtraversoOctoapi@users.noreply.github.com>
2024-04-15 11:39:38 -0400

    Update main.py

--- functions/creditBureau/src/main.py
+++ functions/creditBureau/src/main.py
@@ -1,4 +1,11 @@
 import random
+from appwrite.client import Client
+
+# Initialize the Appwrite client
+client = Client()
+client.set_endpoint('https://cloud.appwrite.io/v1') \
+    .set_project('661bf232a2d367eccb49') \
+    .set_key('320f65e950d95bd0691e1373d8284014d835d8ba310b53180b96685f5df287cddc337e99f770952431dd1186beed8476940b19b001342ba7641a0d4a67d9297a7cfbfd0963579b44828298a308b651e06fc9a4224ab312a957e299233742e8e92e3511d5721926d9636b01afaccdecfeb805dde4af9ba838b6474bf76236a8de')
 
 # Estados posibles para el crédito
 credit_statuses = ["Normal", "Atrasado", "Moroso", "Sin Deudas"]
