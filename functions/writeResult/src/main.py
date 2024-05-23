0ceea6d742f4cca0a164b714d7b656b19732059c
jtraversoOctoapi <167014703+jtraversoOctoapi@users.noreply.github.com>
2024-05-23 18:29:47 -0400

    Update main.py

--- functions/writeResult/src/main.py
+++ functions/writeResult/src/main.py
@@ -5,9 +5,9 @@ import json
 
 # Configuración inicial del cliente de Appwrite
 client = Client()
-client.set_endpoint('https://cloud.appwrite.io/v1')  # Tu endpoint de API
-client.set_project('661bf232a2d367eccb49')  # Tu ID de proyecto
-# Opcional: Configura la sesión si es necesario
+client.set_endpoint('https://cloud.appwrite.io/v1') \
+    .set_project('661bf232a2d367eccb49') \
+    .set_key('68a4559f483223bde0c57f0630be9b37191a1a809353da2538a1dcbc3da07039cdf6e9e09a62ae01baa5f026b241981f7cf95ea4be884a12cd95b23d22baae2cc25540fe12cb9655224b0d5c8b7d7022098e7afa558724718032854c413b0d25d08cd6c8615c61e611c6ad83b225f61e4a5bea40ef1b8b4573343bfea693d58a')
 
 database = Databases(client)
 
