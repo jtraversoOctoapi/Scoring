778d4fbddd2b23c693a9e22644f48a22b966d958
jtraversoOctoapi <167014703+jtraversoOctoapi@users.noreply.github.com>
2024-05-23 17:04:29 -0400

    Update main.py

--- functions/writeResult/src/main.py
+++ functions/writeResult/src/main.py
@@ -1,37 +1,47 @@
 from appwrite.client import Client
-import os
+from appwrite.services.databases import Databases
+from appwrite.exception import AppwriteException
+import json
 
+# Configuración inicial del cliente de Appwrite
+client = Client()
+client.set_endpoint('https://cloud.appwrite.io/v1')  # Tu endpoint de API
+client.set_project('661bf232a2d367eccb49')  # Tu ID de proyecto
+# Opcional: Configura la sesión si es necesario
 
-# This is your Appwrite function
-# It's executed each time we get a request
+database = Databases(client)
+
 def main(context):
-    # Why not try the Appwrite SDK?
-    #
-    # client = (
-    #     Client()
-    #     .set_endpoint("https://cloud.appwrite.io/v1")
-    #     .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-    #     .set_key(os.environ["APPWRITE_API_KEY"])
-    # )
+    # Verificar que el método de la solicitud sea POST
+    if context.req.method != 'POST':
+        return context.res.json({'message': 'Invalid request method, POST required'}, 405)
+    
+    try:
+        # Parsea el JSON del cuerpo de la solicitud
+        body = json.loads(context.req.body)
+        result = body.get('result')
+        path_parts = context.req.path.split('/')
+        
+        if len(path_parts) == 3 and path_parts[1] == 'document_id':
+            document_id = path_parts[2]
+            if document_id and result is not None:
+                try:
+                    # Actualiza el documento en la base de datos
+                    document = database.update_document(
+                        database_id='661c0ff748205b5d00b5',
+                        collection_id='661c1000c15d1c28d50a',
+                        document_id=document_id,
+                        data={'respuesta': result},
+                        permissions=['read("any")', 'write("any")']  # Puedes ajustar los permisos según sea necesario
+                    )
+                    return context.res.json(document, 200)
+                except AppwriteException as e:
+                    context.error(f"Failed to update document: {str(e)}")
+                    return context.res.json({'error': str(e.message)}, 500)
+            else:
+                return context.res.json({'message': 'Document ID and result are required'}, 400)
+        else:
+            return context.res.json({'message': 'Invalid path'}, 400)
+    except Exception as e:
+        return context.res.json({'error': str(e)}, 500)
 
-    # You can log messages to the console
-    context.log("Hello, Logs!")
-
-    # If something goes wrong, log an error
-    context.error("Hello, Errors!")
-
-    # The `ctx.req` object contains the request data
-    if context.req.method == "GET":
-        # Send a response with the res object helpers
-        # `ctx.res.send()` dispatches a string back to the client
-        return context.res.send("Hello, World!")
-
-    # `ctx.res.json()` is a handy helper for sending JSON
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
-    )
