65abc09fcbf94469d79fd9b33f868381ee445308
jtraversoOctoapi <167014703+jtraversoOctoapi@users.noreply.github.com>
2024-05-29 19:14:35 -0400

    Update main.py

--- functions/getResultado/src/main.py
+++ functions/getResultado/src/main.py
@@ -1,14 +1,13 @@
 from appwrite.client import Client
 from appwrite.services.databases import Databases
 from appwrite.exception import AppwriteException
-from .templates import html_template  # Asegúrate de tener una plantilla HTML adecuada para renderizar la salida
 import json
 
 # Configuración inicial del cliente de Appwrite
 client = Client()
-client.set_endpoint('https://cloud.appwrite.io/v1') \
-    .set_project('661bf232a2d367eccb49') \
-    .set_key('68a4559f483223bde0c57f0630be9b37191a1a809353da2538a1dcbc3da07039cdf6e9e09a62ae01baa5f026b241981f7cf95ea4be884a12cd95b23d22baae2cc25540fe12cb9655224b0d5c8b7d7022098e7afa558724718032854c413b0d25d08cd6c8615c61e611c6ad83b225f61e4a5bea40ef1b8b4573343bfea693d58a')
+client.set_endpoint('https://cloud.appwrite.io/v1')
+client.set_project('661bf232a2d367eccb49')
+client.set_key('your_api_key_here')
 
 database = Databases(client)
 
@@ -25,21 +24,27 @@ def main(context):
         return context.res.json({'message': 'Document ID is required'}, 400)
 
     try:
-        # Recupera el documento especificado
         document = database.get_document('661c0ff748205b5d00b5', '661c1000c15d1c28d50a', document_id)
-        response_data = json.loads(document['respuesta']) if isinstance(document['respuesta'], str) else document['respuesta']
+        if 'respuesta' in document:
+            response_data = json.loads(document['respuesta'])
+            basic_info = {key: response_data[key] for key in ['rut', 'email', 'monto', 'plazo', 'Lista Negra', 'Lista Blanca', 'estado_credito', 'total_deudas', 'score_credito']}
+            
+            # Asegurarse de parsear correctamente el campo Resultado Pricing que es una cadena JSON dentro de otra cadena
+            if 'Resultado Pricing' in response_data:
+                pricing_results = json.loads(response_data['Resultado Pricing'])
+            else:
+                pricing_results = []
 
-        # Preparar datos para renderizar en la plantilla HTML
-        basic_info = {key: response_data[key] for key in ['rut', 'email', 'monto', 'plazo', 'Lista Negra', 'Lista Blanca', 'estado_credito', 'total_deudas', 'score_credito']}
-        pricing_results = response_data['Resultado Pricing']
-
-        # Renderizar la página HTML con los datos
-        html_content = html_template.render(basic_info=basic_info, pricing_results=pricing_results)
-        return context.res.send(html_content, 200, {'content-type': 'text/html'})
+            # Utilizar una plantilla HTML adecuada para renderizar la salida
+            html_content = html_template.render(basic_info=basic_info, pricing_results=pricing_results)
+            return context.res.send(html_content, 200, {'content-type': 'text/html'})
+        else:
+            return context.res.json({'message': 'No response data available'}, 404)
 
     except AppwriteException as e:
         return context.res.json({'error': str(e.message)}, 500)
-
+    except json.JSONDecodeError as e:
+        return context.res.json({'error': 'JSON decoding error: ' + str(e)}, 500)
     except Exception as e:
-        return context.res.json({'error': str(e)}, 500)
+        return context.res.json({'error': 'Unhandled exception: ' + str(e)}, 500)
 
