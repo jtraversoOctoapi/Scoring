d927c404abee6632d59cd439a883e3fc1a5c7d39
jtraversoOctoapi <167014703+jtraversoOctoapi@users.noreply.github.com>
2024-06-21 18:39:24 -0400

    Update main.py

--- functions/CRMDemo/src/main.py
+++ functions/CRMDemo/src/main.py
@@ -9,31 +9,29 @@ client.set_project('661bf232a2d367eccb49')         # Asegúrate de cambiar esto 
 client.set_key('68a4559f483223bde0c57f0630be9b37191a1a809353da2538a1dcbc3da07039cdf6e9e09a62ae01baa5f026b241981f7cf95ea4be884a12cd95b23d22baae2cc25540fe12cb9655224b0d5c8b7d7022098e7afa558724718032854c413b0d25d08cd6c8615c61e611c6ad83b225f61e4a5bea40ef1b8b4573343bfea693d58a')  # Asegúrate de cambiar esto por tu API key real
 
 def main(context):
-    if context.req.method != 'GET':
-        return context.res.json({'message': 'Invalid request method, GET required'}, 405)
+    if context.req.method == 'GET':
+        # HTML para el formulario
+        html_content = """
+        <html>
+        <head>
+            <title>Ingrese RUT</title>
+        </head>
+        <body>
+            <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+                <form action="" method="post">
+                    <label for="rut">RUT SIN DV:</label>
+                    <input type="text" id="rut" name="rut" required>
+                    <button type="submit">Enviar</button>
+                </form>
+            </div>
+        </body>
+        </html>
+        """
+        # Usar .send para enviar contenido HTML, especificando el tipo de contenido adecuadamente
+        return context.res.send(html_content, status_code=200, headers={'content-type': 'text/html'})
 
-    # HTML para el formulario
-    html_content = """
-    <html>
-    <head>
-        <title>Ingrese RUT</title>
-    </head>
-    <body>
-        <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-            <form action="" method="post">
-                <label for="rut">RUT SIN DV:</label>
-                <input type="text" id="rut" name="rut" required>
-                <button type="submit">Enviar</button>
-            </form>
-        </div>
-    </body>
-    </html>
-    """
-    return context.res.html(html_content, status_code=200)
-
-    # Manejar POST request
-    if context.req.method == 'POST':
-        rut = context.req.payload.get('rut')
+    elif context.req.method == 'POST':
+        rut = context.req.payload['rut']
 
         # URL del webhook de Make
         webhook_url = 'https://hook.us1.make.com/pfox96c7uh6tch6csu1vhtv8yl7h4qyb'
