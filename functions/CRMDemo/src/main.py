0a5383264391e5b2873d3a49b0b451ca28992722
jtraversoOctoapi <167014703+jtraversoOctoapi@users.noreply.github.com>
2024-06-21 18:30:51 -0400

    Update main.py

--- functions/CRMDemo/src/main.py
+++ functions/CRMDemo/src/main.py
@@ -1,8 +1,15 @@
 from appwrite.client import Client
 from appwrite.services.functions import Functions
+import requests
 
-def main(req, res):
-    if req.method == 'GET':
+# Configuración inicial del cliente de Appwrite
+client = Client()
+client.set_endpoint('https://cloud.appwrite.io/v1') \
+    .set_project('661bf232a2d367eccb49') \
+    .set_key('68a4559f483223bde0c57f0630be9b37191a1a809353da2538a1dcbc3da07039cdf6e9e09a62ae01baa5f026b241981f7cf95ea4be884a12cd95b23d22baae2cc25540fe12cb9655224b0d5c8b7d7022098e7afa558724718032854c413b0d25d08cd6c8615c61e611c6ad83b225f61e4a5bea40ef1b8b4573343bfea693d58a')
+
+def main(context):
+    if context['request']['method'] == 'GET':
         # HTML para el formulario
         html_content = """
         <html>
@@ -20,26 +27,18 @@ def main(req, res):
         </body>
         </html>
         """
-        return res.html(html_content)
-    
-    if req.method == 'POST':
-        rut = req.payload['rut']
+        return context['response'].html(html_content)
 
-        # URL del webhook
+    elif context['request']['method'] == 'POST':
+        rut = context['request']['payload']['rut']
+
+        # URL del webhook de Make
         webhook_url = 'https://hook.us1.make.com/pfox96c7uh6tch6csu1vhtv8yl7h4qyb'
         
-        # Preparar el cliente HTTP y enviar el RUT al webhook de Make
-        response = req.client.post(webhook_url, json={"rut": rut})
+        # Enviar el RUT al webhook de Make
+        response = requests.post(webhook_url, json={"rut": rut})
         
-        if response.status_code == 200:
-            return res.json({'message': 'RUT enviado correctamente'})
+        if response.ok:
+            return context['response'].json({'message': 'RUT enviado correctamente'})
         else:
-            return res.json({'error': 'Error al enviar el RUT'}, status_code=response.status_code)
-
-# Configuración del cliente de Appwrite (ajustar según sea necesario)
-client = Client()
-client.set_endpoint('https://[YOUR_API_ENDPOINT]/v1')  # Tu Endpoint de Appwrite
-client.set_project('[YOUR_PROJECT_ID]')  # Tu Project ID
-client.set_key('[YOUR_API_KEY]')  # Tu API Key
-
-functions = Functions(client)
+            return context['response'].json({'error': 'Error al enviar el RUT'}, status_code=response.status_code)
